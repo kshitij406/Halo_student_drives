aef555d1733221738321ef42e07d5331829f3606
Kshitij_Jha <kshitij.j615@gmail.com>
2025-04-21 22:58:48 +0400

    blue_change_carousel.tsx

--- src/app/components/Carousel.tsx
+++ src/app/components/Carousel.tsx
@@ -12,7 +12,7 @@ export default function Carousel({ services }: { services: Service[] }) {
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-2">
       {services.map((service) => (
         <Link key={service.id} href={`/service/${service.id}`}>
-          <div className="bg-white text-gray-900 text-center py-6 px-4 rounded-xl shadow hover:shadow-xl transition cursor-pointer font-semibold text-lg hover:bg-yellow-100">
+          <div className="bg-blue text-gray-900 text-center py-6 px-4 rounded-xl shadow hover:shadow-xl transition cursor-pointer font-semibold text-lg hover:bg-yellow-100">
             {service.name}
           </div>
         </Link>
