87d70d6f594e76dcd493a11551487c844bfa6b50
Kshitij Jha <kshitij.j615@gmail.com>
2025-04-23 10:02:24 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -75,7 +75,7 @@ export default function HomePage() {
     <main className="p-4 max-w-screen-xl mx-auto text-white">
       <h1 className="px-4 py-2 text-white">Hi, {user?.username || 'Guest'}</h1>
       <h1 className="text-3xl font-bold">Start Riding Now!</h1>
-      <p className="text-gray-400 mb-3">Find a ride service below:</p>
+      <p className="text-gray-400 mb-3">Find a ride service below</p>
 
       {/* Search Bar with Icon */}
       <div className="relative mb-4">
