03da46732b2593191e39f6ce73f1ed6089986c29
Kshitij Jha <kshitij.j615@gmail.com>
2025-04-24 08:54:02 +0530

    vercel fix

--- src/app/dev/requests/[id]/page.tsx
+++ src/app/dev/requests/[id]/page.tsx
@@ -1,10 +1,16 @@
 import DriverReviewPageClient from "./ClientPage";
 
-export default function Page({ params }: { params: { id: string } }) {
+interface PageParams {
+  params: {
+    id: string;
+  };
+}
+
+export default function Page({ params }: PageParams) {
   return <DriverReviewPageClient id={params.id} />;
 }
 
-export async function generateMetadata({ params }: { params: { id: string } }) {
+export async function generateMetadata({ params }: PageParams) {
   return {
     title: `Driver Approval - ${params.id}`,
   };
