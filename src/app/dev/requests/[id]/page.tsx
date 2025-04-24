a5011dd8381f45470a8cbcb302d4d240c439f594
Kshitij Jha <kshitij.j615@gmail.com>
2025-04-24 08:56:32 +0530

    Update page.tsx

--- src/app/dev/requests/[id]/page.tsx
+++ src/app/dev/requests/[id]/page.tsx
@@ -1,17 +1,12 @@
+import { Metadata } from "next";
 import DriverReviewPageClient from "./ClientPage";
 
-interface PageParams {
-  params: {
-    id: string;
-  };
-}
-
-export default function Page({ params }: PageParams) {
-  return <DriverReviewPageClient id={params.id} />;
-}
-
-export async function generateMetadata({ params }: PageParams) {
+export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
   return {
-    title: `Driver Approval - ${params.id}`,
+    title: `Driver Review - ${params.id}`,
   };
+}
+
+export default function Page({ params }: { params: { id: string } }) {
+  return <DriverReviewPageClient id={params.id} />;
 }
